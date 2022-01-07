5257f063bb73f8a6d077da162aa9a3d431597ac6
Jean Carlos <jeaanca.santos@gmail.com>
2022-01-07 20:57:16 -0300

    first commit

---
+++ lib/scitree.ex
@@ -0,0 +1,18 @@
+defmodule Scitree do
+  @moduledoc """
+  Documentation for `Scitree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scitree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/scitree_test.exs
@@ -0,0 +1,8 @@
+defmodule ScitreeTest do
+  use ExUnit.Case
+  doctest Scitree
+
+  test "greets the world" do
+    assert Scitree.hello() == :world
+  end
+end
