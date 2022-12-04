3a2b8824e75bd5c66b68d34a4f0db32017810373
JeanTux <jeaanca.santos@gmail.com>
2022-12-04 19:56:52 -0300

    add format

--- mix.exs
+++ mix.exs
@@ -11,9 +11,9 @@ defmodule Scitree.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       compilers: [:elixir_make] ++ Mix.compilers(),
-
       make_precompiler: {:nif, CCPrecompiler},
-      make_precompiler_url: "https://github.com/jeantux/scitree/releases/download/v#{@version}/@{artefact_filename}",
+      make_precompiler_url:
+        "https://github.com/jeantux/scitree/releases/download/v#{@version}/@{artefact_filename}",
       make_precompiler_filename: "scitree",
       make_precompiler_priv_paths: ["scitree.*"]
     ]
@@ -31,7 +31,7 @@ defmodule Scitree.MixProject do
     [
       {:elixir_make, "~> 0.6", runtime: false, github: "elixir-lang/elixir_make", override: true},
       {:cc_precompiler, "~> 0.1.0", runtime: false, github: "cocoa-xu/cc_precompiler"},
-      {:nx, "~> 0.1.0", override: true},
+      {:nx, "~> 0.1.0", override: true}
     ]
   end
 end
